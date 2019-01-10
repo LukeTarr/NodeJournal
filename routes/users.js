fdeb83dfa5ea9b458218046455cb139866836b94
Luke Tarr <lrt22@pitt.edu>
2019-01-09 22:12:43 -0500

    added errors partial

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,43 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 //User Login Route
-router.get('/login', (req, res) =>{
-    res.render('users/login');
-  })
-  
-  //User Register Route
-  router.get('/register', (req, res) =>{
-    res.render('users/register');
-  })
-  
+router.get('/login', (req, res) => {
+  res.render('users/login');
+})
 
-module.exports = router;
+//User Register Route
+router.get('/register', (req, res) => {
+  res.render('users/register');
+})
+
+//Register Form POST
+router.post('/register', (req, res) => {
+      let errors = [];
+
+      if (req.body.password != req.body.password2) {
+        errors.push({
+          text: 'Passwords do not match'
+        });
+      }
+
+      if (req.body.password.length < 4) {
+        errors.push({
+          text: 'Password must be at least 4 characters'
+        });
+      }
+
+      if (errors.length > 0) {
+        res.render('users/register', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            password2: req.body.password2
+          });
+    } else{
+      res.send('passed');
+    }
+});
+
+
+    module.exports = router;
